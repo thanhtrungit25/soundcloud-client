de3036729687761aca8149e7f16c83dff13a6eed
TrungDT <thanhtrung.it25@gmail.com>
2017-10-15 11:36:50 +0700

    Add react-hot-loader config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
 
     output: {
         path: __dirname + '/build',
@@ -11,7 +15,8 @@ module.exports = {
     },
 
     devServer: {
-        contentBase: './build'
+        contentBase: './build',
+        hot: true
     },
 
     plugins: [
